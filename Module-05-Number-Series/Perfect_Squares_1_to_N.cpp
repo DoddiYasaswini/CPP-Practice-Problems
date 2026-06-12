9782679798c36132096ca967471298beb3a090b5
doddiyasaswini <doddiyasaswini686@gmail.com>
2026-06-12 15:12:09 +0530

    Add program to print perfect squares up to N

---
+++ Module-05-Number-Series/Perfect_Squares_1_to_N.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    for (int i = 1; i * i <= n; i++) {
+        cout << i * i << " ";
+    }
+
+    return 0;
+}
