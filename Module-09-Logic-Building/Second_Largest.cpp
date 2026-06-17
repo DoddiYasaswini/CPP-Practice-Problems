64bf4cebdc5c1e6bf114189493909e41629b95e6
doddiyasaswini <doddiyasaswini686@gmail.com>
2026-06-17 11:47:06 +0530

    Create Second_Largest.cpp

---
+++ Module-09-Logic-Building/Second_Largest.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for(int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+
+        if(x > largest) {
+            secondLargest = largest;
+            largest = x;
+        }
+        else if(x > secondLargest && x != largest) {
+            secondLargest = x;
+        }
+    }
+
+    cout << secondLargest;
+
+    return 0;
+}
