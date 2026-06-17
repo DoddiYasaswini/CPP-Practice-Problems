7209f511774ddc2fdc5f74c7227c39e5ba3d68c8
doddiyasaswini <doddiyasaswini686@gmail.com>
2026-06-17 11:49:24 +0530

    Create Missing_Number.cpp

---
+++ Module-09-Logic-Building/Missing_Number.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int sum = 0;
+
+    for(int i = 0; i < n - 1; i++) {
+        int x;
+        cin >> x;
+        sum += x;
+    }
+
+    int expected = n * (n + 1) / 2;
+
+    cout << expected - sum;
+
+    return 0;
+}
